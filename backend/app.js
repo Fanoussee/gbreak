c2fa7b5f5ec50c7cacbb59cd24f351826c3c4cfb
Fanoussee <stephanie.t16@laposte.net>
2021-01-17 10:17:17 +0100

    Ajout de la requête pour obtenir tous les utilisateurs.

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,20 @@ const app = express();
 //Récupération de la connexion avec la base de données MySQL gbreak
 const connexion = require('./connect');
 
+//Import du package body-parser
+const bodyParser = require('body-parser');
+
+//Résoud les erreurs de CORS
+app.use(function(req, res, next){
+    res.setHeader("Access-Control-Allow-Origin",  "*" ) ;
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization") ;
+    res.setHeader("Access-Control-Allow-Methods" , "GET, POST, PUT, DELETE, PATCH, OPTIONS") ;
+    next() ;
+}) ;
+
+//Utilisation de body-parser pour l'app
+app.use(bodyParser.json());
+
 //Se connecter à la base de données
 connexion.connect(function(error){
     if(error){
@@ -15,12 +29,23 @@ connexion.connect(function(error){
     console.log('Connecté au serveur MySQL.');
 });
 
+//Requête pour obtenir tous les utilisateurs
+app.get("/api/utilisateurs", function(req, res){
+    const sql = 'SELECT * FROM Utilisateur';
+    connexion.query(sql, function(err, rows, fields){
+        if(err){
+            res.status(500).json({ error : err.message });
+        }
+        res.json(rows);
+    });
+});
+
 //Se déconnecter à la base de données
-connexion.end(function(error){
+/*connexion.end(function(error){
     if(error){
         return console.error('error : ' + error.message);
     }
     console.log('Déconnecté du serveur MySQL.');
-});
+});*/
 
 module.exports = app;
