ba59390ed39450a3633d94053178349635b314ad
Fanoussee <stephanie.t16@laposte.net>
2021-01-17 20:14:47 +0100

    Ajout de la fonctionnalité obtenir un article avec son identifiant.

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -11,3 +11,23 @@ exports.getAllArticles = function(req, res){
         }
     });
 }
+
+exports.getOneArticleWithId = function(req, res){
+    const sql = 'SELECT * FROM Article WHERE id_article=?';
+    const idArticle = req.params.idArticle;
+    let result = 0;
+    connexion.query(sql, [idArticle], function(err, rows, fields){
+        if(err){
+            res.status(500).json({ erreur : "La requête est incorrecte !"});
+        }else{
+            try {
+                result = rows[0].id_article;
+                if(result == idArticle){
+                    res.status(200).json(rows);
+                }
+            } catch (error) {
+                res.status(500).json({ erreur : "L'article n'existe pas !"});
+            }
+        }
+    });
+}
--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -5,5 +5,6 @@ const router = express.Router();
 const ctrlArticles = require("../controllers/articles");
 
 router.get("/", ctrlArticles.getAllArticles);
+router.get("/:idArticle", ctrlArticles.getOneArticleWithId);
 
 module.exports = router;
