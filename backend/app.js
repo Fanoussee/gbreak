e650c853c37c72ff47bc3e7c2b1ba31928e0fa0c
Fanoussee <stephanie.t16@laposte.net>
2021-01-17 11:24:48 +0100

    Résolution des erreurs de texte de requête et pour un utilisateur qui n'existe pas dans la base de données.

--- backend/app.js
+++ backend/app.js
@@ -11,46 +11,67 @@ const connexion = require('./connect');
 const bodyParser = require('body-parser');
 
 //Résoud les erreurs de CORS
-app.use(function(req, res, next){
-    res.setHeader("Access-Control-Allow-Origin",  "*" ) ;
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization") ;
-    res.setHeader("Access-Control-Allow-Methods" , "GET, POST, PUT, DELETE, PATCH, OPTIONS") ;
-    next() ;
-}) ;
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    next();
+});
 
 //Utilisation de body-parser pour l'app
 app.use(bodyParser.json());
 
 //Se connecter à la base de données
-connexion.connect(function(error){
-    if(error){
+connexion.connect(function (error) {
+    if (error) {
         return console.error('error : ' + error.message);
     }
     console.log('Connecté au serveur MySQL.');
 });
 
 //Requête pour obtenir tous les utilisateurs
-app.get("/api/utilisateurs", function(req, res){
+app.get("/api/utilisateurs", function (req, res) {
     const sql = 'SELECT * FROM Utilisateur';
-    connexion.query(sql, function(err, rows, fields){
-        if(err){
-            res.status(500).json({ error : err.message });
+    connexion.query(sql, function (err, rows, fields) {
+        if (err) {
+            res.status(500).json({ error: err.message });
         }
         res.json(rows);
     });
 });
 
 //Requête pour obtenir un utilisateur
-app.get("/api/utilisateurs/:id", function(req, res){
+app.get("/api/utilisateurs/:id", function (req, res) {
     const sql = 'SELECT * FROM Utilisateur WHERE id_util=?';
     const idUtil = req.params.id;
-    connexion.query(sql, [idUtil], function(err, rows, fields){
-        if(err){
-            res.status(500).json({ error : err.message });
+    let result = 0;
+    connexion.query(sql, [idUtil], function (err, rows, fields) {
+        if (err) {
+            res.status(500).json({ erreur: "La requête est incorrecte !" });
+        } else {
+            try {
+                result = rows[0].id_util;
+                if (result == idUtil) {
+                    res.status(200).json(rows);
+                }
+            } catch (error) {
+                res.status(500).json({ erreur: "L'utilisateur n'existe pas !" });
+            }
         }
-        res.json(rows);
     });
 });
+
+//Requête pour supprimer un utilisateur
+app.get("/api/utilisateurs/:id", function (req, res) {
+    const sql = 'DELETE FROM Utilisateur WHERE id_util=?';
+    const idUtil = req.params.id;
+    connexion.query(sql, [idUtil], function (err, rows, fields) {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        }
+        res.json({ message: "Utilisateur supprimé !" });
+    })
+})
 
 //Se déconnecter à la base de données
 /*connexion.end(function(error){
