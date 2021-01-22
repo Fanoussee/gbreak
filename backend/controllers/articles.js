ed7ed8e45eb4c0ac686cae8e86cd62b5b399ee64
Fanoussee <stephanie.t16@laposte.net>
2021-01-22 15:47:42 +0100

    Ajout de la fonctionnalité supprimer un article.

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -40,17 +40,17 @@ exports.getAllArticlesForOneUser = function (req, res) {
     connexion.query(sql1, [idUtil], function (err, rows, fields) {
         if (err) {
             res.status(500).json({ erreur: "La requête est incorrecte !" });
-        }else{
-            try{
+        } else {
+            try {
                 result = rows[0].id_util;
-                if(idUtil == result){
-                    connexion.query(sql2, [idUtil], function(err, rows, fields){
-                        if(err){
-                            res.status(500).json({ erreur : "La requête est incorrecte !" });
-                        }else{
+                if (idUtil == result) {
+                    connexion.query(sql2, [idUtil], function (err, rows, fields) {
+                        if (err) {
+                            res.status(500).json({ erreur: "La requête est incorrecte !" });
+                        } else {
                             try {
                                 result = rows[0].id_util;
-                                if(idUtil == result){
+                                if (idUtil == result) {
                                     res.status(200).json(rows);
                                 }
                             } catch (error) {
@@ -59,8 +59,33 @@ exports.getAllArticlesForOneUser = function (req, res) {
                         }
                     });
                 }
-            }catch(error){
-                res.status(500).json({ erreur : "L'utilisateur n'existe pas !" });
+            } catch (error) {
+                res.status(500).json({ erreur: "L'utilisateur n'existe pas !" });
+            }
+        }
+    });
+}
+
+exports.deleteArticle = function (req, res) {
+    const sql1 = 'SELECT * FROM Article WHERE id_article=?';
+    const sql2 = 'DELETE FROM Article WHERE id_article=?';
+    const idArticle = req.params.idArticle;
+    let result = 0;
+    connexion.query(sql1, [idArticle], function (err, rows, fields) {
+        if (err) {
+            res.status(500).json({ erreur: "La requête est incorrecte !" });
+        } else {
+            try {
+                result = rows[0].id_article;
+                connexion.query(sql2, [idArticle], function (err, rows, fields) {
+                    if (err) {
+                        res.status(500).json({ erreur: "La requête est incorrecte !" });
+                    } else {
+                        res.status(200).json({ message: "L'article a été supprimé !" });
+                    }
+                });
+            } catch (error) {
+                res.status(500).json({ erreur: "L'article n'existe pas !" });
             }
         }
     });
--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -7,5 +7,6 @@ const ctrlArticles = require("../controllers/articles");
 router.get("/", ctrlArticles.getAllArticles);
 router.get("/:idArticle", ctrlArticles.getOneArticleWithId);
 router.get("/all/:idUtil", ctrlArticles.getAllArticlesForOneUser);
+router.delete("/:idArticle", ctrlArticles.deleteArticle);
 
 module.exports = router;
