8648dd79c9b8968b38d10d1b3d07a71c1e608173
Fanoussee <stephanie.t16@laposte.net>
2021-01-22 16:38:29 +0100

    Ajout de la fonctionnalité créer un article.

--- backend/controllers/articles.js
+++ backend/controllers/articles.js
@@ -91,27 +91,60 @@ exports.deleteArticle = function (req, res) {
     });
 }
 
-exports.modifyArticle = function(req, res){
+exports.modifyArticle = function (req, res) {
     const sql1 = 'SELECT * FROM Article WHERE id_article=?';
     const sql2 = 'UPDATE Article SET ? WHERE id_article=?';
     const idArticle = req.params.idArticle;
     const values = req.body;
     let result = 0;
-    connexion.query(sql1, [idArticle], function(err, rows, fields){
-        if(err){
-            res.status(500).json({ erreur : "La requête est incorrecte ! " });
-        }else{
+    connexion.query(sql1, [idArticle], function (err, rows, fields) {
+        if (err) {
+            res.status(500).json({ erreur: "La requête est incorrecte ! " });
+        } else {
             try {
                 result = rows[0].id_article;
-                connexion.query(sql2, [values, idArticle], function(err, rows, fields){
-                    if(err){
-                        res.status(500).json({ erreur : "La requête est incorrecte ! " });
-                    }else{
-                        res.status(200).json({ message : "L'article a été modifié !" });
+                connexion.query(sql2, [values, idArticle], function (err, rows, fields) {
+                    if (err) {
+                        res.status(500).json({ erreur: "La requête est incorrecte ! " });
+                    } else {
+                        res.status(200).json({ message: "L'article a été modifié !" });
                     }
                 });
             } catch (error) {
                 res.status(500).json({ erreur: "L'article n'existe pas !" });
+            }
+        }
+    });
+}
+
+exports.createArticle = function (req, res) {
+    const sql1 = 'SELECT * FROM Utilisateur WHERE id_util=?';
+    const sql2 = 'INSERT INTO Article (id_util, date_heure, photo, texte, nb_commentaires) VALUES (?,?,?,?,?)';
+    const idUtil = req.body.id_util;
+    const values = req.body;
+    let result = 0;
+    let dateCreation = new Date();
+    connexion.query(sql1, [idUtil], function (err, rows, fields) {
+        if (err) {
+            res.status(500).json({ erreur: "La requête est incorrecte !" });
+        } else {
+            try {
+                result = rows[0].id_util;
+                if (values.texte == null && values.photo == null) {
+                    res.status(500).json({ erreur: "L'article ne peut pas être vide !" });
+                } else {
+                    connexion.query(sql2,
+                        [values.id_util, dateCreation, values.photo, values.texte, 0],
+                        function (err, rows, fields) {
+                            if (err) {
+                                res.status(500).json({ erreur: "La requête est incorrecte !" });
+                            } else {
+                                res.status(200).json({ message: "L'article a été ajouté !" });
+                            }
+                        });
+                }
+            } catch (error) {
+                res.status(500).json({ erreur: "L'utilisateur n'existe pas !" });
             }
         }
     });
--- backend/routes/articles.js
+++ backend/routes/articles.js
@@ -9,5 +9,6 @@ router.get("/:idArticle", ctrlArticles.getOneArticleWithId);
 router.get("/all/:idUtil", ctrlArticles.getAllArticlesForOneUser);
 router.delete("/:idArticle", ctrlArticles.deleteArticle);
 router.put("/:idArticle", ctrlArticles.modifyArticle);
+router.post("/", ctrlArticles.createArticle);
 
 module.exports = router;
